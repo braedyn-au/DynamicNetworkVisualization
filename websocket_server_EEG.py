601bda9b7f0d40523b8f28a813e5589be20ad503
Braedyn Au <37196122+braedyn-au@users.noreply.github.com>
2020-11-28 03:34:10 -0700

    recovery
    recovery

--- websocket_server_EEG.py
+++ websocket_server_EEG.py
@@ -6,10 +6,12 @@ from tornado import websocket, web, ioloop
 from datetime import timedelta
 from random import randint
 import csv
+import pandas as pd
+import os
 
 paymentTypes = ["cash", "tab", "visa","mastercard","bitcoin"]
 namesArray = ['Ben', 'Jarrod', 'Vijay', 'Aziz']
-#
+
 class WebSocketHandler(websocket.WebSocketHandler):
 	
 	def check_origin(self, origin):
@@ -31,15 +33,21 @@ class WebSocketHandler(websocket.WebSocketHandler):
 		print "Sending Data"
 		#create a bunch of random data for various dimensions we want
 		k = 0
-		with open("Smaller_EEGfile.csv") as csvfile:
-			csv_reader = csv.reader(csvfile, delimiter=",")
-			for row in csv_reader:		
+		# with open("Smaller_EEGfile.csv") as csvfile:
+		for net in os.listdir('./nets'):
+			print "./nets/" + str(net)
+			with open("./nets/"+net) as csvfile:
+				# csv_reader = csv.reader(csvfile, delimiter=",")
+				csv_reader = pd.read_csv(csvfile, header=0)
+				#threshold values
+				csv_reader = csv_reader[csv_reader['weight']>0.9]
+				# print csv_reader['node1']
 				#create a new data point
 				point_data = {
-					'value': abs(float(row[4]))
+					'node1': list(csv_reader['node1']),
+					'node2': list(csv_reader['node2']),
+					'weight': list(csv_reader['weight']) #abs(float(row[4]))
 				}
-
-				print point_data
 
 				#write the json object to the socket
 				self.write_message(json.dumps(point_data))
